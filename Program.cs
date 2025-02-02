1c041193f2cf17632b40eda4d1b8cf1d10fb458d
20100 <mmmelkady23@gmail.com>
2025-02-02 17:59:55 +0200

    First one

--- Program.cs
+++ Program.cs
@@ -1,10 +1,47 @@
-﻿namespace Advanced_C__2
+﻿using System.Collections;
+
+namespace Advanced_C__2
 {
     internal class Program
     {
+        #region First One
+        public static void Swap(int x, int y, ArrayList arr)
+        {
+            object? Temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = Temp;
+
+        }
+        public static void MyReverse(ArrayList arr)
+        {
+            int start = 0;
+            int end = arr.Count - 1;
+            while (start < end)
+            {
+                Swap(start, end, arr);
+                start++;
+                end--;
+            }
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region First-Point
+            ArrayList names = new ArrayList();
+            names.Add("Mohamed Hesham");
+            names.Add("omar");
+            names.Add("ali");
+            names.Add("Amr");
+            names.Add(2025);
+            MyReverse(names);
+            //********************
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
         }
     }
 }
