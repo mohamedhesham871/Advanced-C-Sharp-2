45b6e24cb857cf622c122a90f9e643d625c3b2bf
20100 <mmmelkady23@gmail.com>
2025-02-02 19:17:12 +0200

    Second

--- Program.cs
+++ Program.cs
@@ -25,22 +25,43 @@ namespace Advanced_C__2
         }
         #endregion
 
+        #region Second
+        static public List<int> Even(List<int> num)
+        {
+            List<int> list = new List<int>();
+            for(int i=0;i<num.Count;i++)
+            {
+                if (num[i]%2==0)
+                    list.Add(num[i]);
+            }
+            return list;
+        }
+        #endregion
         static void Main(string[] args)
         {
             #region First-Point
-            ArrayList names = new ArrayList();
-            names.Add("Mohamed Hesham");
-            names.Add("omar");
-            names.Add("ali");
-            names.Add("Amr");
-            names.Add(2025);
-            MyReverse(names);
-            //********************
-            foreach (var item in names)
-            {
-                Console.WriteLine(item);
-            }
+            //ArrayList names = new ArrayList();
+            //names.Add("Mohamed Hesham");
+            //names.Add("omar");
+            //names.Add("ali");
+            //names.Add("Amr");
+            //names.Add(2025);
+            //MyReverse(names);
+            ////********************
+            //foreach (var item in names)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
+            #endregion
+
+            #region Second-Point
+            List<int> number = new List<int>();
+            number.AddRange([1,2,3,4,5,6,7,8,9,10]);
+            List<int>EvenNumber= Even(number);
+
+            foreach(int item in EvenNumber)
+                Console.WriteLine(item);
             #endregion
         }
     }
